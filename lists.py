8464e2ca1114b3eff2058e1fa4e2159b13447e20
arcbabu <archana.masterseducation@gmail.com>
2020-04-07 15:02:41 -0500

    Basics for working with lists

---
+++ lists.py
@@ -0,0 +1,21 @@
+# Basics of working with lists
+
+fruits = ["Apple","Orange","Grape","Watermelon","Strawberry"]
+# index      0       1        2        3             4
+# reverse   -5      -4       -3       -2            -1
+apple = ["Apple","red", 4]
+
+print(fruits)
+print(apple)
+
+# Printing by its index value
+print(fruits[1])
+
+# Printing values from backwards. Starts from -1, -2, -3 and so on
+print(fruits[-2])
+
+# Printing values from index to all after that
+print(fruits[2:])
+
+# Printing values using the range
+print(fruits[2:4])
