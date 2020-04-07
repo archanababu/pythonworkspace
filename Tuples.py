50ad630b9c78e8c31e27300f8b46e28832917bf9
arcbabu <archana.masterseducation@gmail.com>
2020-04-07 16:02:25 -0500

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,13 @@
+#--------
+#TUPLES - Type of data structure. Its a container where we can store different types of values.
+#         Very similar to list but there are some difference which will be shown below
+#         Tuples are immutable - cant be modified once initalized.
+#         Tuples contains coordinates (x,y). Starting at index 0. Its uses paranthesis 
+#--------
+
+coordinates = (4,5)
+print(coordinates[1])
+
+
+
+
