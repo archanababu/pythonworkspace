cb46b5bdd15af797378a5d013567273a5e2de09c
arcbabu <archana.masterseducation@gmail.com>
2020-04-07 19:12:41 -0500

    Translate function using for loop and if condition

---
+++ translation.py
@@ -0,0 +1,27 @@
+#---------------------------
+# VOWEL translation function
+#---------------------------
+
+'''
+You can type multi line comments here
+For example1
+example2
+example3
+'''
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    
+    return translation
+
+newPhrase = input("Enter a new phrase : ")
+
+print(translate(newPhrase))
