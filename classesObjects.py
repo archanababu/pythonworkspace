7bce96adb37ee62838f4e9d73f10eb5904efb023
arcbabu <archana.masterseducation@gmail.com>
2020-04-08 22:19:03 -0500

    Classes and objects in python

---
+++ classesObjects.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------
+# Classes and Objects
+# - Create your own data type using class 
+# - Define your own data type
+#
+#-------------------------------------------------------------
+
+from student import Student
+
+student1 = Student("1111", "Anna", "Business", 3.1, False)
+student2 = Student("2222", "Alex", "Computer Science", 3.8, False)
+print(student2.name)
---
+++ student.py
@@ -0,0 +1,8 @@
+class Student:
+     def __init__(self, studentID, name, major, gpa, isOnProbation):
+         self.studentID = studentID
+         self.name = name
+         self.major = major
+         self.gpa = gpa
+         self.isOnProbation = isOnProbation
+
